120296f385967d71a29ed5adffd6630ae298312b
Satish Marisetty <satishmarisetti79@gmail.com>
2023-09-08 22:30:00 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     debugShowCheckedModeBanner: false,
     theme: ThemeData.dark().copyWith(
       primaryColor: Color(0xFF0a0d22),
-      scaffoldBackgroundColor: Color(0xef0a0d22),
+      scaffoldBackgroundColor: Color(0xff0a0d22),
     ),
     home: BMI(),
   ));
@@ -26,6 +26,77 @@ class _BMiState extends State<BMI> {
         centerTitle: true,
         backgroundColor: Color(0xff0a0d22),
         title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+              child: Row(
+                 children: <Widget>[
+                   Expanded(
+                       child: Container(
+                         margin: EdgeInsets.all(15),
+                         decoration: BoxDecoration(
+                           color: Color(0xff1d1e33),
+                           borderRadius: BorderRadius.circular(10),
+                         ),
+
+                         //child: Text(''),
+                       )
+                   ),
+                   Expanded(
+                       child: Container(
+                         margin: EdgeInsets.all(15),
+                         decoration: BoxDecoration(
+                           color: Color(0xff1d1e33),
+                           borderRadius: BorderRadius.circular(10),
+                         ),
+                         //child: Text(''),
+                       )
+                   ),
+                 ],
+              ),
+          ),
+          Expanded(
+            flex: 1,
+              child:
+              Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color(0xff1d1e33),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                //child: Text(''),
+              )
+          ),
+          Expanded(
+            flex: 1,
+              child:Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        //child: Text(''),
+                      )
+                  ),
+                  Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          color: Color(0xff1d1e33),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        //child: Text(''),
+                      )
+                  ),
+                ],
+              ),
+          ),
+        ],
       ),
     );
   }
